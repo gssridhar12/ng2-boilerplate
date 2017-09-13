90a1fdb0a752ff227f2040a71a12d4eaa575f6a4
Sridhar <sridhar.cse12@gmail.com>
2017-09-13 23:00:25 +0530

    imp files

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div class="container">
+      <h4>App Component</h4>
+    </div>
+  `
+})
+
+export class AppComponent{}
